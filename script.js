beff4d48e6885b7239acd7e209f48f7a4e6cc4ca
Alex Ebbage <Alexebbage@gmail.com>
2021-05-10 14:24:31 +0100

    Add the accommodation and a single walk

--- script.js
+++ script.js
@@ -17,23 +17,7 @@ function setupMap(){
     map.addSource("places", {
       // This GeoJSON contains features that include an "icon" property. The value of the "icon" property corresponds to an image in the Mapbox Streets style's sprite.
       type: "geojson",
-      data: {
-        type: "FeatureCollection",
-        features: [
-          {
-            type: "Feature",
-            properties: {
-              description:
-                '<strong>Make it Mount Pleasant</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
-              icon: "theatre-15",
-            },
-            geometry: {
-              type: "Point",
-              coordinates: [-77.038659, 38.931567],
-            },
-          },
-        ],
-      },
+      data: "./features.geojson"
     });
 
     // Add a layer showing the places.
