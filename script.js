1cc1d131f6ee6e830e93d8f3efe935559aad27a5
Alex Ebbage <Alexebbage@gmail.com>
2021-05-10 11:05:50 +0100

    Added navigation controls to the map

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var map = null;
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWJiYWdlIiwiYSI6ImNrb2llYmd2bTFlcHIybnRyOW9odGFld28ifQ.zmALWbpNj2NMcbPaYJHG1g';
 
 setupMap();
 
 function setupMap(){
-  map = new mapboxgl.Map({
+  const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/streets-v11",
     center: [-3.0946354, 54.433919],
     zoom: 9.8,
   });
+
+  const navigation = new mapboxgl.NavigationControl();
+  map.addControl(navigation);
 }
